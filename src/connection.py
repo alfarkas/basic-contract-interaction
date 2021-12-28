a2659fba4d60b555036171422d99941e01d26d5c
farkas <ariel.farkas@ripio.com>
2021-12-28 11:40:00 -0300

    use new contract

--- src/connection.py
+++ src/connection.py
@@ -7,11 +7,11 @@ from web3.middleware import geth_poa_middleware
 load_dotenv()
 
 abi = json.loads(open("src/contract_abi.json").read())
+
 w3 = Web3(HTTPProvider(os.getenv("PROVIDER", "https://matic-mumbai.chainstacklabs.com")))
 w3.middleware_onion.inject(geth_poa_middleware, layer=0)
-print(w3.isConnected())
+contract_address = w3.toChecksumAddress(os.getenv("CONTRACT_ADDR", "0xd9E0b2C0724F3a01AaECe3C44F8023371f845196"))
 
-contract_address = "0x94494F7129b2Eb4c3D21bDF80700E963e07179cd"
-created_block = 22366976 # contract creation block
+created_block = 22660777 # contract creation block
 
 contract = w3.eth.contract(address=contract_address, abi=abi)
