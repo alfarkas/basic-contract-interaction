0b7f3323716e53e5615c20ec58590309951b30f6
farkas <ariel.farkas@ripio.com>
2022-01-11 15:18:50 -0300

    added service to sign transactions

---
+++ service.py
@@ -0,0 +1,38 @@
+# stdlib
+import json
+import os
+
+# flask
+from flask import Flask, request, jsonify
+
+# deps
+from dotenv import load_dotenv
+
+# local
+from src.connection import w3
+
+
+app = Flask(__name__)
+
+load_dotenv()
+
+
+@app.route("/")
+def sign():
+    tx = json.loads(request.get_json())
+    if not tx:
+        return jsonify({"error": "A transaction must be provided"})
+    key = os.environ["KEY"]
+    try:
+        signed_tx = w3.eth.account.sign_transaction(tx, key)
+    except Exception as e:
+        return jsonify({"error": str(e.args[0])})
+    return jsonify(
+        {
+            "rawTransaction": signed_tx["rawTransaction"].hex(),
+            "hash": signed_tx["hash"].hex(),
+            "r": signed_tx["r"],
+            "s": signed_tx["s"],
+            "v": signed_tx["v"],
+        }
+    )
--- src/products.py
+++ src/products.py
@@ -1,22 +1,25 @@
+# stdlib
+import json
+import os
+
 # deps
+import requests
 from web3 import exceptions as web3Exceptions
 
 # local
-from src.connection import contract, created_block, w3
+from src.connection import contract, w3
 from src.models import Product
 
 from .exceptions import ProductDoesNotExists
 
 
-def send_transaction(transaction, acc_address, key):
+def send_transaction(transaction, acc_address):
     """Build a transaction and send it.
 
     :param transaction: transaction function
     :type transaction: function
     :param acc_address: address to send the transaction from
     :type acc_address: str
-    :param key: private key from the address
-    :type key: str
     """
     try:
         tx = transaction.buildTransaction(
@@ -27,65 +30,66 @@ def send_transaction(transaction, acc_address, key):
                 "nonce": w3.eth.get_transaction_count(acc_address),
             }
         )
-        signed_tx = w3.eth.account.sign_transaction(tx, key)
-
-        hash_tx = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
     except web3Exceptions.InvalidAddress:
         return {"error": "Invalid address"}
+    try:
+        # call microservice to sign tx
+        signed_tx = requests.request(
+            "GET",
+            os.getenv("SIGN_URL"),
+            headers={"content-type": "application/json"},
+            json=json.dumps(tx),
+        ).json()
+
+        hash_tx = w3.eth.send_raw_transaction(signed_tx["rawTransaction"])
     except Exception as e:
         print(e)
         return {"error": "Something went wrong, try again."}
     return hash_tx
 
 
-def create_product(name, acc_address, key):
+def create_product(name, acc_address):
     """Creates a new product with a given name from an address as owner.
 
     :param name: name of product
     :type name: str
     :param acc_address: account address
     :type acc_address: str
-    :param key: private key of the account
-    :type key: str
     :return: hash of the transaction
     :rtype: str
     """
     tx = contract.functions.createProduct(name)
-    return send_transaction(tx, acc_address, key)
+    return send_transaction(tx, acc_address)
 
 
-def delegate_product(product_id, acc_address, key, acc2_address):
+def delegate_product(product_id, acc_address, acc2_address):
     """Delegates a product to another account
 
     :param product_id: id of the product
     :type product_id: int
     :param acc_address: account address that makes the operation
     :type acc_address: str
-    :param key: private key of the account
-    :type key: str
     :param acc2_address: account address of the new owner
     :type acc2_address: str
     :return: hash of the transaction
     :rtype: str
     """
     tx = contract.functions.delegateProduct(product_id, acc2_address)
-    return send_transaction(tx, acc_address, key)
+    return send_transaction(tx, acc_address)
 
 
-def accept_product(product_id, acc_address, key):
+def accept_product(product_id, acc_address):
     """Accepts a product ownership
 
     :param product_id: id of the product
     :type product_id: int
     :param acc_address: account address of the new owner
     :type acc_address: str
-    :param key: private key of the account
-    :type key: str
     :return: hash of the transaction
     :rtype: str
     """
     tx = contract.functions.acceptProduct(product_id)
-    return send_transaction(tx, acc_address, key)
+    return send_transaction(tx, acc_address)
 
 
 def get_product(product_id):
